bfec4a645c427d2b2ceedd5b21e364aac3e70ce7
Marta Jankowska <martajan218@gmail.com>
2020-08-15 17:32:56 +0200

    finished

--- project-management-client/src/components/tasks/EditTask.js
+++ project-management-client/src/components/tasks/EditTask.js
@@ -9,11 +9,59 @@ class EditTask extends Component {
             description: this.props.theTask.description
         }
     }
+
+    handleTaskFormSubmit = event => {
+        const title = this.state.title;
+        const description = this.state.description;
+
+        event.preventDefault();
+
+        axios
+            .put(`http://localhost:4000/api/tasks/${this.props.theTask._id}`, {
+                title,
+                description
+            })
+            .then(() => {
+                this.props.getTheTask();
+                this.props.history.push('/tasks');
+            })
+            .catch(error => console.log(error));
+    }
+
+    handleChangeTaskTitle = event => {
+        this.setState({
+            title: event.target.value
+        })
+    }
+
+    handleChangeTaskDesc = event => {
+        this.setState({
+            description: event.target.value
+        })
+    }
     
     render() {
         return (
             <div>
-                
+                <hr />
+                <h3>Edit form</h3>
+                <form onSubmit={this.handleTaskFormSubmit}>
+                  <label>Title:</label>
+                  <input
+                    type="text"
+                    name="title"
+                    value={this.state.title}
+                    onChange={e => this.handleChangeTaskTitle(e)}
+                  />
+                  <label>Description:</label>
+                  <textarea
+                    name="description"
+                    value={this.state.description}
+                    onChange={e => this.handleChangeTaskDesc(e)}
+                  />
+        
+                  <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
--- project-management-client/src/components/tasks/TaskDetails.js
+++ project-management-client/src/components/tasks/TaskDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import EditTask from './EditTask'
 
 
 class TaskDetails extends Component {
@@ -26,12 +27,33 @@ class TaskDetails extends Component {
     })
   }
 
+  renderEditTaskForm = () => {
+    if(!this.state.title){
+        this.getTheTask();
+    } else {
+        return <EditTask theTask={this.state} getTheTask={this.getSingleTask} {...this.props} />
+    }
+  }
+
+  deleteTask = () => {
+    const { params } = this.props.match;
+    axios.delete(`http://localhost:4000/api/tasks/${params.taskId}`)
+    .then( () =>{
+        this.props.history.push('/tasks');
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+}
+
   render(){
     return(
       <div>
         <h3>TASK DETAILS</h3>
         <h2>{this.state.title}</h2>
         <p>{this.state.description}</p> 
+        <div>{this.renderEditTaskForm()} </div>
+        <button onClick={() => this.deleteTask()}>Delete task</button>
         <button onClick={this.props.history.goBack}>Go Back</button>
       </div>
     )
